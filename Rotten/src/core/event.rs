3037560a20682a207372de854cd4866e7aea7bf5
Manuel Sellers Rico <manuel.sellers.rico@everis.com>
2022-11-30 11:11:59 +0100

    Implement event casting

--- Rotten/src/core/event.rs
+++ Rotten/src/core/event.rs
@@ -1,3 +1,5 @@
+use std::any::Any;
+
 use bitflags::bitflags;
 
 macro_rules! event_functions {
@@ -22,7 +24,7 @@ macro_rules! event_functions {
 
 macro_rules! code_function {
     () => {
-        fn code(&self) -> u32 {
+        pub fn code(&self) -> u32 {
             self.event.code()
         }
     }
@@ -53,13 +55,13 @@ pub enum EventType {
 
 bitflags! {
     pub struct EventCategory: u16 {
-        const None = 0x00;
+        const NONE = 0x00;
 
-        const Application = 0x01;
-        const Input = 0x02;
-        const Keyboard = 0x04;
-        const Mouse = 0x08;
-        const MouseButton = 0x10;
+        const APPLICATION = 0x01;
+        const INPUT = 0x02;
+        const KEYBOARD = 0x04;
+        const MOUSE = 0x08;
+        const MOUSE_BUTTON = 0x10;
     }
 }
 
@@ -73,7 +75,7 @@ pub trait EventTrait {
     }
 
     fn in_category(&self, category: &EventCategory) -> bool {
-        (*self.event_category() & *category) != EventCategory::None
+        (*self.event_category() & *category) != EventCategory::NONE
     }
 }
 
@@ -99,10 +101,6 @@ impl Event {
 }
 
 impl EventTrait for Event {
-    fn in_category(&self, category: &EventCategory) -> bool {
-        (self.event_category & *category) != EventCategory::None
-    }
-
     fn event_type(&self) -> &EventType {
         &self.event_type
     }
@@ -113,6 +111,10 @@ impl EventTrait for Event {
 
     fn name(&self) -> &str {
         "Event"
+    }
+
+    fn in_category(&self, category: &EventCategory) -> bool {
+        (self.event_category & *category) != EventCategory::NONE
     }
 }
 
@@ -128,7 +130,7 @@ impl EventTrait for KeyEvent {
 
 impl KeyEvent {
     pub fn new(code: u32, event_type: EventType) -> KeyEvent {
-        let event = Event::new(event_type, EventCategory::Keyboard | EventCategory::Input);
+        let event = Event::new(event_type, EventCategory::KEYBOARD | EventCategory::INPUT);
 
         KeyEvent {
             event,
@@ -203,7 +205,7 @@ impl EventTrait for MouseMoveEvent {
 
 impl MouseMoveEvent {
     pub fn new(x: f64, y: f64) -> MouseMoveEvent {
-        let event = Event::new(EventType::MouseMove, EventCategory::Input & EventCategory::Mouse);
+        let event = Event::new(EventType::MouseMove, EventCategory::INPUT & EventCategory::MOUSE);
 
         MouseMoveEvent {
             event,
@@ -234,7 +236,7 @@ impl EventTrait for MouseScrolledEvent {
 
 impl MouseScrolledEvent {
     pub fn new(x_offset: f64, y_offset: f64) -> MouseScrolledEvent {
-        let event = Event::new(EventType::MouseScrolled, EventCategory::Input & EventCategory::Mouse);
+        let event = Event::new(EventType::MouseScrolled, EventCategory::INPUT & EventCategory::MOUSE);
 
         MouseScrolledEvent {
             event,
@@ -256,7 +258,7 @@ impl EventTrait for MouseButtonEvent {
 
 impl MouseButtonEvent {
     pub fn new(code: u32, event_type: EventType) -> MouseButtonEvent {
-        let event = Event::new(event_type, EventCategory::MouseButton | EventCategory::Input);
+        let event = Event::new(event_type, EventCategory::MOUSE_BUTTON | EventCategory::INPUT);
 
         MouseButtonEvent {
             event,
@@ -324,7 +326,7 @@ impl EventTrait for WindowResizeEvent {
 
 impl WindowResizeEvent {
     fn new(width: u32, height: u32) -> WindowResizeEvent {
-        let event = Event::new(EventType::WindowResize, EventCategory::Application);
+        let event = Event::new(EventType::WindowResize, EventCategory::APPLICATION);
 
         WindowResizeEvent {
             event,
@@ -344,8 +346,8 @@ impl EventTrait for WindowCloseEvent {
 }
 
 impl WindowCloseEvent {
-    fn new(width: u32, height: u32) -> WindowCloseEvent {
-        let event = Event::new(EventType::WindowClose, EventCategory::Application);
+    fn new() -> WindowCloseEvent {
+        let event = Event::new(EventType::WindowClose, EventCategory::APPLICATION);
 
         WindowCloseEvent {
             event
@@ -363,8 +365,8 @@ impl EventTrait for AppTickEvent {
 }
 
 impl AppTickEvent {
-    fn new(width: u32, height: u32) -> AppTickEvent {
-        let event = Event::new(EventType::AppTick, EventCategory::Application);
+    fn new() -> AppTickEvent {
+        let event = Event::new(EventType::AppTick, EventCategory::APPLICATION);
 
         AppTickEvent {
             event
@@ -382,8 +384,8 @@ impl EventTrait for AppRenderEvent {
 }
 
 impl AppRenderEvent {
-    fn new(width: u32, height: u32) -> AppRenderEvent {
-        let event = Event::new(EventType::AppRender, EventCategory::Application);
+    fn new() -> AppRenderEvent {
+        let event = Event::new(EventType::AppRender, EventCategory::APPLICATION);
 
         AppRenderEvent {
             event
@@ -401,8 +403,8 @@ impl EventTrait for AppUpdateEvent {
 }
 
 impl AppUpdateEvent {
-    fn new(width: u32, height: u32) -> AppUpdateEvent {
-        let event = Event::new(EventType::AppUpdate, EventCategory::Application);
+    fn new() -> AppUpdateEvent {
+        let event = Event::new(EventType::AppUpdate, EventCategory::APPLICATION);
 
         AppUpdateEvent {
             event
@@ -412,39 +414,38 @@ impl AppUpdateEvent {
 
 // TODO
 pub trait EventDispatcher {
-    fn dispatch<T: EventTrait>(&self, event: T) {
-        match event {
-            Event::event_type(EventType::None) => self.event(event),
-            EventType::None => self.event(event),
-            EventType::WindowClose => self.window_close_event(event),
-            EventType::WindowResize => self.window_resize_event(event),
-            EventType::WindowFocus => self.event(event),
-            EventType::WindowLostFocus => self.event(event),
-            EventType::WindowMoved => self.event(event),
-            EventType::AppTick => self.app_tick_event(event),
-            EventType::AppUpdate => self.app_update_event(event),
-            EventType::AppRender => self.app_render_event(event),
-            EventType::KeyPressed => self.key_pressed_event(event),
-            EventType::KeyReleased => self.key_released_event(event),
-            EventType::MouseButtonPressed => self.mouse_button_pressed_event(event),
-            EventType::MouseButtonReleased => self.mouse_button_released_event(event),
-            EventType::MouseMove => self.mouse_move_event(event),
-            EventType::MouseScrolled => self.mouse_scrolled_event(event),
+    fn dispatch<T: EventTrait + 'static>(&self, event: T) {
+        match event.event_type() {
+            EventType::None => self.event((&event as &dyn Any).downcast_ref::<Event>().unwrap()),
+            EventType::WindowClose => self.window_close_event((&event as &dyn Any).downcast_ref::<WindowCloseEvent>().unwrap()),
+            EventType::WindowResize => self.window_resize_event((&event as &dyn Any).downcast_ref::<WindowResizeEvent>().unwrap()),
+            EventType::WindowFocus => self.event((&event as &dyn Any).downcast_ref::<Event>().unwrap()),
+            EventType::WindowLostFocus => self.event((&event as &dyn Any).downcast_ref::<Event>().unwrap()),
+            EventType::WindowMoved => self.event((&event as &dyn Any).downcast_ref::<Event>().unwrap()),
+            EventType::AppTick => self.app_tick_event((&event as &dyn Any).downcast_ref::<AppTickEvent>().unwrap()),
+            EventType::AppUpdate => self.app_update_event((&event as &dyn Any).downcast_ref::<AppUpdateEvent>().unwrap()),
+            EventType::AppRender => self.app_render_event((&event as &dyn Any).downcast_ref::<AppRenderEvent>().unwrap()),
+            EventType::KeyPressed => self.key_pressed_event((&event as &dyn Any).downcast_ref::<KeyPressedEvent>().unwrap()),
+            EventType::KeyReleased => self.key_released_event((&event as &dyn Any).downcast_ref::<KeyReleasedEvent>().unwrap()),
+            EventType::MouseButtonPressed => self.mouse_button_pressed_event((&event as &dyn Any).downcast_ref::<MouseButtonPressedEvent>().unwrap()),
+            EventType::MouseButtonReleased => self.mouse_button_released_event((&event as &dyn Any).downcast_ref::<MouseButtonReleasedEvent>().unwrap()),
+            EventType::MouseMove => self.mouse_move_event((&event as &dyn Any).downcast_ref::<MouseMoveEvent>().unwrap()),
+            EventType::MouseScrolled => self.mouse_scrolled_event((&event as &dyn Any).downcast_ref::<MouseScrolledEvent>().unwrap()),
         }
     }
 
-    fn event(&self, event: Event) {}
-    fn key_event(&self, event: KeyEvent) {}
-    fn key_pressed_event(&self, event: KeyPressedEvent) {}
-    fn key_released_event(&self, event: KeyEvent) {}
-    fn mouse_move_event(&self, event: MouseMoveEvent) {}
-    fn mouse_scrolled_event(&self, event: MouseScrolledEvent) {}
-    fn mouse_button_event(&self, event: MouseButtonEvent) {}
-    fn mouse_button_pressed_event(&self, event: MouseButtonPressedEvent) {}
-    fn mouse_button_released_event(&self, event: MouseButtonReleasedEvent) {}
-    fn window_resize_event(&self, event: WindowResizeEvent) {}
-    fn window_close_event(&self, event: WindowCloseEvent) {}
-    fn app_tick_event(&self, event: AppTickEvent) {}
-    fn app_render_event(&self, event: AppRenderEvent) {}
-    fn app_update_event(&self, event: AppUpdateEvent) {}
+    fn event(&self, event: &Event) {}
+    fn key_event(&self, event: &KeyEvent) {}
+    fn key_pressed_event(&self, event: &KeyPressedEvent) {}
+    fn key_released_event(&self, event: &KeyReleasedEvent) {}
+    fn mouse_move_event(&self, event: &MouseMoveEvent) {}
+    fn mouse_scrolled_event(&self, event: &MouseScrolledEvent) {}
+    fn mouse_button_event(&self, event: &MouseButtonEvent) {}
+    fn mouse_button_pressed_event(&self, event: &MouseButtonPressedEvent) {}
+    fn mouse_button_released_event(&self, event: &MouseButtonReleasedEvent) {}
+    fn window_resize_event(&self, event: &WindowResizeEvent) {}
+    fn window_close_event(&self, event: &WindowCloseEvent) {}
+    fn app_tick_event(&self, event: &AppTickEvent) {}
+    fn app_render_event(&self, event: &AppRenderEvent) {}
+    fn app_update_event(&self, event: &AppUpdateEvent) {}
 }
--- Sandbox/src/main.rs
+++ Sandbox/src/main.rs
@@ -1,12 +1,12 @@
 use rotten::core::Application;
-use rotten::core::event::KeyPressedEvent;
+use rotten::core::event::{EventTrait, KeyPressedEvent};
 
 fn main() {
     let app = Application{};
 
     let event = KeyPressedEvent::new(42, 0);
 
-    println!("name: {}", event.name());
+    println!("name: {}, code: {}", event.name(), event.code());
 
     app.run();
 }
